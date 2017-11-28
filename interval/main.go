1b955be97f3ca7afc4b6e9b6d7cb981db2ad54c7
Nepthis <viktorcarlsson9@gmail.com>
2017-11-28 10:02:02 +0100

    getall & getbyzonename somewhat done

--- interval/main.go
+++ interval/main.go
@@ -35,12 +35,20 @@ type PostRes struct {
 
 type Interval struct {
 	Id      bson.ObjectId `json:"id" bson:"_id,omitempty"`
+	DeviceMAC string `json:"devicemac"`
+	SensorMAC string `json:"sensormac"`
+	from uint64 `json:"from"` 
+	to uint64 `json:"to"`
+	zone string `json:zone`
+
+}
+type Zone struct {
+	Id      bson.ObjectId `json:"id" bson:"_id,omitempty"`
 	Name string `json:"name"`
 	Location []float64 `json:"location"`
 	Children []bson.ObjectId	 `json:"children" bson:"children,omitempty"`
 	Parent bson.ObjectId `json:"parent" bson:"parent,omitempty"`
 }
-
 type Instances struct {
 	Session *mgo.Session
 	Consul *api.Client
@@ -97,16 +105,40 @@ func main() {
 	mux := goji.NewMux()
 	mux.HandleFunc(pat.Get("/intervals"), allIntervals(instances))
 	mux.HandleFunc(pat.Get("/intervals/:at"), getIntervalByTime(instances))
-	mux.HandleFunc(pat.Get("/intervals/zones/:zoneid"), getIntervalByZoneID(instances))
-	mux.HandleFunc(pat.Get("/intervals/zones/:zoneid, :at"), getIntervalByZoneIDDuringInterval(instances))
-	mux.HandleFunc(pat.Get("/intervals/zones/:deviceid"), getIntervalByDeviceID(instances))
+	mux.HandleFunc(pat.Get("/intervals/zones/:zonename"), getIntervalByZoneName(instances))
+	mux.HandleFunc(pat.Get("/intervals/zones/:zonename/:from/:to"), getIntervalByZoneIDDuringInterval(instances))
+	mux.HandleFunc(pat.Get("/intervals/device/:deviceid"), getIntervalByDeviceID(instances))
+	mux.HandleFunc(pat.Get("/intervals/device/:zonename"), getIntervalForDeviceInZone(instances))
 	mux.HandleFunc(pat.Get("/intervals/zones/:at"), getIntervalForZoneByTime(instances))
 	mux.HandleFunc(pat.Get("/"), healthCheck(instances))
 	fmt.Printf("Starting Router\n")
     http.ListenAndServe("0.0.0.0:8080", mux)
 
 }
-
+func readFromZones(){
+	resp, err := http.Get("http.se.com.muuuuuu.nuuuuuuuujust 	")
+	if err != nil {
+		failOnError(err, "Read from Zones failed")
+	}
+	defer resp.Body.Close()
+	body, err := ioutil.ReadAll(resp.Body)
+}
+func readFromZonesMeta(){
+	resp, err := http.Get("http://example.com/")
+	if err != nil {
+		failOnError(err, "Read from Zone Metadata failed")
+	}
+	defer resp.Body.Close()
+	body, err := ioutil.ReadAll(resp.Body)
+}
+func readFromDataStore(){
+	resp, err := http.Get("http://example.com/")
+	if err != nil {
+		failOnError(err, "Read from Datastore failed")
+	}
+	defer resp.Body.Close()
+	body, err := ioutil.ReadAll(resp.Body)
+}
 func healthCheck(i *Instances) func(w http.ResponseWriter, r *http.Request) {  
     return func(w http.ResponseWriter, r *http.Request) {
 		requestDump, err := httputil.DumpRequest(r, false)
@@ -115,14 +147,14 @@ func healthCheck(i *Instances) func(w http.ResponseWriter, r *http.Request) {
 		
 		buffer := new(bytes.Buffer)
 
-		response := PostRes{Success: "true", Message: "I am alive"}
+		response := PostRes{Success: true, Message: "I am alive"}
 		json.NewEncoder(buffer).Encode(response)
 		respBody := buffer.Bytes()
 		ResponseWithJSON(w, respBody, http.StatusOK)
 	}
 }
 
-func allZones(i *Instances) func(w http.ResponseWriter, r *http.Request) {  
+func allIntevals(i *Instances) func(w http.ResponseWriter, r *http.Request) {  
     return func(w http.ResponseWriter, r *http.Request) {
 		requestDump, err := httputil.DumpRequest(r, false)
 		fmt.Println(string(requestDump))
@@ -135,19 +167,19 @@ func allZones(i *Instances) func(w http.ResponseWriter, r *http.Request) {
 		session := i.Session.Copy()
         defer session.Close()
 
-        c := session.DB("store").C("zones")
+        c := session.DB("store").C("intervals")
 
-        zones := make([]Zone, 0)
-        err = c.Find(bson.M{}).All(&zones)
+        intervals := make([]Interval, 0)
+        err = c.Find(bson.M{}).All(&intervals)
         if err != nil {
-			response = PostRes{Success: "false", Message: "Database error"}
+			response = PostRes{Success: false, Message: "Database error"}
 			json.NewEncoder(buffer).Encode(response)
 			respBody := buffer.Bytes()
 			ResponseWithJSON(w, respBody, http.StatusInternalServerError)
             return
         }
 		
-		respBody, err = json.MarshalIndent(zones, "", "  ")
+		respBody, err = json.MarshalIndent(intervals, "", "  ")
 		if err != nil {
 			log.Fatal(err)
 		}
@@ -155,45 +187,134 @@ func allZones(i *Instances) func(w http.ResponseWriter, r *http.Request) {
         ResponseWithJSON(w, respBody, http.StatusOK)
     }
 }
-func ensureIndex(s *mgo.Session) {  
-    session := s.Copy()
-    defer session.Close()
-
-    c := session.DB("store").C("books")
-
-    index := mgo.Index{
-        Key:        []string{"isbn"},
-        Unique:     true,
-        DropDups:   true,
-        Background: true,
-        Sparse:     true,
-    }
-    err := c.EnsureIndex(index)
-    if err != nil {
-        panic(err)
-    }
-}
-
-func allBooks(s *mgo.Session) func(w http.ResponseWriter, r *http.Request) {  
+func getIntervalByTime(i *Instances) func(w http.ResponseWriter, r *http.Request) {  
     return func(w http.ResponseWriter, r *http.Request) {
-        session := s.Copy()
-        defer session.Close()
+		requestDump, err := httputil.DumpRequest(r, false)
+		fmt.Println(string(requestDump))
+		failOnError(err, string(requestDump))
 
-        c := session.DB("store").C("books")
+        session := i.Session.Copy()
+		defer session.Close()
 
-        var books []Book
-        err := c.Find(bson.M{}).All(&books)
+		buffer := new(bytes.Buffer)
+		var response PostRes
+		var respBody []byte
+
+		at := bson.ObjectIdHex(pat.Param(r, "at"))
+
+        c := session.DB("store").C("intervals")
+//IF TIMESTAMP.FROM << REQUESTED && TIMESTAMP.TO >> REQUESTED = intervallet överlappar den sökta tiden
+        var interval Interval
+        err = c.Find(bson.M{"_name": name}).One(&interval)
         if err != nil {
-            ErrorWithJSON(w, "Database error", http.StatusInternalServerError)
-            log.Println("Failed get all books: ", err)
-            return
+			response = PostRes{Success: false, Message: "Database error"}
+			json.NewEncoder(buffer).Encode(response)
+			respBody := buffer.Bytes()
+			ResponseWithJSON(w, respBody, http.StatusInternalServerError)
+			return
         }
-
-        respBody, err := json.MarshalIndent(books, "", "  ")
+		
+        if interval.name == "" {
+			response = PostRes{Success: false, Message: "Interval not found"}
+			json.NewEncoder(buffer).Encode(response)
+			respBody := buffer.Bytes()
+			ResponseWithJSON(w, respBody, http.StatusInternalServerError)
+			return
+		}
+//IF TIMESTAMP.FROM << REQUESTED && TIMESTAMP.TO >> REQUESTED = intervallet överlappar den sökta tiden
+        respBody, err = json.MarshalIndent(interval, "", "  ")
         if err != nil {
             log.Fatal(err)
         }
 
         ResponseWithJSON(w, respBody, http.StatusOK)
     }
+}
+func getIntervalByZoneName(i *Instances) func(w http.ResponseWriter, r *http.Request) {  
+    return func(w http.ResponseWriter, r *http.Request) {
+		requestDump, err := httputil.DumpRequest(r, false)
+		fmt.Println(string(requestDump))
+		failOnError(err, string(requestDump))
+
+        session := i.Session.Copy()
+		defer session.Close()
+
+		buffer := new(bytes.Buffer)
+		var response PostRes
+		var respBody []byte
+
+		name := bson.ObjectIdHex(pat.Param(r, "zonename"))
+
+        c := session.DB("store").C("intervals")
+
+        var interval Interval
+        err = c.Find(bson.M{"_name": name}).One(&interval)
+        if err != nil {
+			response = PostRes{Success: false, Message: "Database error"}
+			json.NewEncoder(buffer).Encode(response)
+			respBody := buffer.Bytes()
+			ResponseWithJSON(w, respBody, http.StatusInternalServerError)
+			return
+        }
+		
+        if interval.name == "" {
+			response = PostRes{Success: false, Message: "Interval not found"}
+			json.NewEncoder(buffer).Encode(response)
+			respBody := buffer.Bytes()
+			ResponseWithJSON(w, respBody, http.StatusInternalServerError)
+			return
+		}
+
+        respBody, err = json.MarshalIndent(interval, "", "  ")
+        if err != nil {
+            log.Fatal(err)
+        }
+
+        ResponseWithJSON(w, respBody, http.StatusOK)
+    }
+}
+func getIntervalByDeviceID(i *Instances) func(w http.ResponseWriter, r *http.Request) {  
+    return func(w http.ResponseWriter, r *http.Request) {
+		requestDump, err := httputil.DumpRequest(r, false)
+		fmt.Println(string(requestDump))
+		failOnError(err, string(requestDump))
+		buffer := new(bytes.Buffer)
+		var response PostRes
+		var respBody []byte
+
+		session := i.Session.Copy()
+        defer session.Close()
+
+        c := session.DB("store").C("intervals")
+	}
+}
+func getIntervalByZoneIDDuringInterval(i *Instances) func(w http.ResponseWriter, r *http.Request) {  
+    return func(w http.ResponseWriter, r *http.Request) {
+		requestDump, err := httputil.DumpRequest(r, false)
+		fmt.Println(string(requestDump))
+		failOnError(err, string(requestDump))
+		buffer := new(bytes.Buffer)
+		var response PostRes
+		var respBody []byte
+
+		session := i.Session.Copy()
+        defer session.Close()
+
+        c := session.DB("store").C("intervals")
+	}
+}
+func getIntervalForZoneByTime(i *Instances) func(w http.ResponseWriter, r *http.Request) {  
+    return func(w http.ResponseWriter, r *http.Request) {
+		requestDump, err := httputil.DumpRequest(r, false)
+		fmt.Println(string(requestDump))
+		failOnError(err, string(requestDump))
+		buffer := new(bytes.Buffer)
+		var response PostRes
+		var respBody []byte
+
+		session := i.Session.Copy()
+        defer session.Close()
+
+        c := session.DB("store").C("intervals")
+	}
 }
