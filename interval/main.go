a4c5d6f821dd10d8e6e8536be5cf4016040398e1
Nepthis <viktorcarlsson9@gmail.com>
2017-12-05 13:31:25 +0100

    recieve skeleton

--- interval/main.go
+++ interval/main.go
@@ -15,7 +15,7 @@ import (
 	"github.com/hashicorp/consul/api"
 	"github.com/streadway/amqp"
 	"time"
-	"io/ioutil"
+	//"io/ioutil"
 	"strconv"
 )
 
@@ -378,7 +378,7 @@ func getIntervalForDeviceInZone(i *Instances) func(w http.ResponseWriter, r *htt
 		var response PostRes
 		var respBody []byte
 
-		name := bson.ObjectIdHex(pat.Param(r, "zonename"))
+		name := (pat.Param(r, "zonename"))
 		deviceid := bson.ObjectIdHex(pat.Param(r, "deviceid"))
 
         c := session.DB("store").C("intervals")
@@ -434,7 +434,7 @@ func getIntervalByZoneNameDuringInterval(i *Instances) func(w http.ResponseWrite
 	        panic(err)
 	    }
 	    tmto := time.Unix(y, 0)
-		zonename := bson.ObjectIdHex(pat.Param(r, "zonename"))
+		zonename := (pat.Param(r, "zonename"))
 
         c := session.DB("store").C("intervals")
         var timeIntervals []Interval
@@ -486,7 +486,7 @@ func getIntervalForZoneByTime(i *Instances) func(w http.ResponseWriter, r *http.
 	        panic(err)
 	    }
 	    tm := time.Unix(i, 0)
-		zonename := bson.ObjectIdHex(pat.Param(r, "zonename"))
+		zonename := (pat.Param(r, "zonename"))
 
         c := session.DB("store").C("intervals")
         var timeIntervals []Interval
@@ -518,4 +518,72 @@ func getIntervalForZoneByTime(i *Instances) func(w http.ResponseWriter, r *http.
 
         ResponseWithJSON(w, respBody, http.StatusOK)
     }
+}
+func (i *Instances) Recieve() {
+        conn, err := amqp.Dial(i.rabbitEndpoint)
+        failOnError(err, "Failed to connect to RabbitMQ")
+        defer conn.Close()
+
+        channel, err := conn.Channel()
+        failOnError(err, "Failed to open a channel")
+        defer channel.Close()
+
+        err = channel.ExchangeDeclare(
+                i.ExchangeTopic, // name
+                "topic",      // type
+                true,         // durable
+                false,        // auto-deleted
+                false,        // internal
+                false,        // no-wait
+                nil,          // arguments
+        )
+        failOnError(err, "Failed to declare an exchange")
+
+        q, err := channel.QueueDeclare(
+                "",    // name
+                false, // durable
+                false, // delete when usused
+                true,  // exclusive
+                false, // no-wait
+                nil,   // arguments
+        )
+        failOnError(err, "Failed to declare a queue")
+
+        if len(os.Args) < 2 {
+                log.Printf("Usage: %s [binding_key]...", os.Args[0])
+                os.Exit(0)
+        }
+        for _, s := range os.Args[1:] {
+                log.Printf("Binding queue %s to exchange %s with routing key %s",
+                        q.Name, "logs_topic", s)
+                err = ch.QueueBind(
+                        q.Name,       // queue name
+                        s,            // routing key
+                        "logs_topic", // exchange
+                        false,
+                        nil)
+                failOnError(err, "Failed to bind a queue")
+        }
+
+        msgs, err := ch.Consume(
+                q.Name, // queue
+                "",     // consumer
+                true,   // auto ack
+                false,  // exclusive
+                false,  // no local
+                false,  // no wait
+                nil,    // args
+        )
+        failOnError(err, "Failed to register a consumer")
+
+        forever := make(chan bool)
+
+        go func() {
+                for d := range msgs {
+                        log.Printf(" [x] %s", d.Body)
+                }
+        }()
+
+        log.Printf(" [*] Waiting for logs. To exit press CTRL+C")
+        <-forever
 }
