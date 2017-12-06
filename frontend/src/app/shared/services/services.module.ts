33fd165a323a3ebb480ebb08f11fc0c09a6b5207
David Rutqvist <david@rutqvist.com>
2017-12-06 16:22:03 +0100

    Install unit (#30)
    * Started implementing sensors service
    
    * Added install unit functionality
    
    * Increased version
    
    * Removed backup files (garbage) and increased app version

--- frontend/src/app/shared/services/services.module.ts
+++ frontend/src/app/shared/services/services.module.ts
@@ -6,11 +6,12 @@ import { SpinnerService } from "./spinner/spinner.service";
 import { DataService } from "./data/data.service";
 import { ThemesService } from "./themes/themes.service";
 import { ZonesService } from "app/shared/services/zones/zones.service";
+import { SensorsService } from "./sensors/sensors.service";
 
 
 @NgModule({
 	imports: [],
-	providers: [ConfigService, ThemesService, PreloaderService, SpinnerService, DataService, ZonesService],
+	providers: [ConfigService, ThemesService, PreloaderService, SpinnerService, DataService, ZonesService, SensorsService],
 	declarations: [],
 	exports: []
 })
